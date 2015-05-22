276b06074c2a0bb13d7ce0d96a050cf971e09fa7
Zoltan Olah <zol@percolatestudio.com>
2015-05-22 12:18:48 -0700

    Turns lint comment warning into error when running git hook

--- meteor/tests/lint.sh
+++ meteor/tests/lint.sh
@@ -6,4 +6,4 @@
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # Will exit with status 1 for errors, 0 for pass and warnings
-exec eslint $BASE_DIR/ $@
+exec eslint --rule "no-warning-comments: 2" $BASE_DIR/ $@
